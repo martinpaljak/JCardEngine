a67d93aba3a9b179f07a72b9990419d55ad6c941
Martin Paljak <martin@martinpaljak.net>
2026-02-10 20:41:12 +0000

    Defensively clone byte[] in SCPConfig record constructors

--- simulator/src/main/java/pro/javacard/engine/globalplatform/SCPConfig.java
+++ simulator/src/main/java/pro/javacard/engine/globalplatform/SCPConfig.java
@@ -22,11 +22,13 @@ public sealed interface SCPConfig permits SCPConfig.SCP02, SCPConfig.SCP03 {
     byte[] masterKey();
 
     record SCP02(byte[] masterKey) implements SCPConfig {
+        public SCP02 { masterKey = masterKey.clone(); }
         public SCP02() { this(PlaintextKeys.DEFAULT_KEY()); }
         @Override public byte[] masterKey() { return masterKey.clone(); }
     }
 
     record SCP03(byte[] masterKey, boolean s16) implements SCPConfig {
+        public SCP03 { masterKey = masterKey.clone(); }
         public SCP03()             { this(PlaintextKeys.DEFAULT_KEY(), false); }
         public SCP03(byte[] key)   { this(key, false); }
         public SCP03(boolean s16)  { this(PlaintextKeys.DEFAULT_KEY(), s16); }
