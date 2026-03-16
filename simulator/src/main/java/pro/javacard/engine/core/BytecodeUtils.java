625819365fcc23fb6dd67e3f4ae193648a2e8d94
Martin Paljak <martin@martinpaljak.net>
2026-03-16 09:32:30 +0200

    Parse class files directly for parent information, instead of loading as classes

--- simulator/src/main/java/pro/javacard/engine/core/BytecodeUtils.java
+++ simulator/src/main/java/pro/javacard/engine/core/BytecodeUtils.java
@@ -5,6 +5,11 @@ import org.objectweb.asm.ClassWriter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UncheckedIOException;
+import java.util.HashSet;
+
 public final class BytecodeUtils {
     private static final Logger log = LoggerFactory.getLogger(BytecodeUtils.class);
 
@@ -33,6 +38,40 @@ public final class BytecodeUtils {
         @Override
         protected ClassLoader getClassLoader() {
             return classLoader;
+        }
+
+        // Resolve common superclass without actually loading classes, but directly parsing class files.
+        // This avoids ClassCircularityError when the parent implementation would call again transform() during COMPUTE_FRAMES.
+        @Override
+        protected String getCommonSuperClass(String type1, String type2) {
+            var ancestors = new HashSet<String>();
+            var current = type1;
+            while (current != null) {
+                ancestors.add(current);
+                current = getSuperClass(current);
+            }
+            current = type2;
+            while (current != null) {
+                if (ancestors.contains(current)) {
+                    return current;
+                }
+                current = getSuperClass(current);
+            }
+            return "java/lang/Object";
+        }
+
+        private String getSuperClass(String type) {
+            if ("java/lang/Object".equals(type)) {
+                return null;
+            }
+            try (InputStream is = classLoader.getResourceAsStream(type + ".class")) {
+                if (is == null) {
+                    throw new TypeNotPresentException(type.replace('/', '.'), null);
+                }
+                return new ClassReader(is).getSuperName();
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
         }
     }
 }
